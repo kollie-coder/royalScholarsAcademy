1dd2d562c01c07e1a3377f0a0b3c2aa663e5c67a
Emmanuel <apotiolasegun@gmail.com>
2023-08-24 11:03:27 +0100

    updated2

--- src/components/dashInfo/Dashinfo.jsx
+++ src/components/dashInfo/Dashinfo.jsx
@@ -1,90 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./dashInfo.scss";
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider, { CSVExport, Search } from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 const { ExportCSVButton } = CSVExport;
 const { SearchBar } = Search;
-const products = [
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
-  { RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2020/2021", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2022/2023", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2022/2023", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2022/2023", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2022/2023", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2233", StudentName: "KOLA SAMUEL", Class: "PRIMARY -2", Session: "2021/2022", Term: "First Term"},
-  {RegNo: "RSA2373", StudentName: "KOLA SAMUEL", Class: "PRIMARY -1", Session: "2021/2022", Term: "First Term"},
- 
-];
-const columns = [
- 
-  
-{
-    dataField: "RegNo",
-    text: "Reg. No",
-    sort: true
-  },
-  {
-    dataField: "StudentName",
-    text: "Student Name",
-    sort: true
-  },
-  {
-    dataField: "Class",
-    text: "Class",
-    sort: true
-  },
-  {
-    dataField: "Session",
-    text: "Session",
-    sort: true,
+
+
     
-  },
-  {
-    dataField: "Term",
-    text: "Term",
-    sort: true
-  },
-  
-  
-  {
-    dataField: "action",
-    text: "Action",
-    headerStyle: (colum, colIndex) => {
-        return { width: "100px", textAlign: "center" };
-       },
-    formatter: (cellContent, row) => {
-      return (
-        <>
-        <button
-        className="" style={{color:'red', padding: '5px', backgroundColor:"white", border:'none'}} >
-        View
-      </button>
-      </>
-      );
-    },
-  },
-  
-  
-  
-];
-
-
-      
-
 const Dashinfo = ()  => {
 
-   
+  const [data1, setData1] = useState([]);
 
+  const products = [
+    {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
+    {RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
+    { RegNo: "RSA2301", StudentName: "OWOLABI SAMUEL", Class: "PLAYGROUP", Session: "2018/2019", Term: "First Term"},
+    
+  ];
+  const columns = [
+   
+    
+  {
+      dataField: "matric",
+      text: "Reg. No",
+      sort: true
+    },
+    {
+      dataField: "fullName",
+      text: "Student Name",
+      sort: true
+    },
+    {
+      dataField: "class_name",
+      text: "Class",
+      sort: true
+    },
+    {
+      dataField: "session_name",
+      text: "Session",
+      sort: true,
+      
+    },
+    {
+      dataField: "term_name",
+      text: "Term",
+      sort: true
+    },
+    
+    
+    {
+      dataField: "action",
+      text: "Action",
+      headerStyle: (colum, colIndex) => {
+          return { width: "100px", textAlign: "center" };
+         },
+      formatter: (cellContent, row) => {
+        return (
+          <>
+          <button
+              onClick={(e) => window.location.href = row.doc}
+          
+          className="" style={{color:'red', padding: '5px', backgroundColor:"white", border:'none'}} >
+          View
+        </button>
+        </>
+        );
+      },
+    },
+    
+    
+    
+  ];
+
+  const fullName = (cell, row) => {
+    return `${row.last_name} ${row.first_name}`;
+  };
+
+  columns[2].formatter = fullName;
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response1 = await axios.get("http://francisop.pythonanywhere.com/school/result/");
+        setData1(response1.data);
+        console.log(data1);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+    }, []);
+  
           
   return (
     <div className="box-mainDi">
@@ -95,7 +105,7 @@ const Dashinfo = ()  => {
   <div className="dashboardTable">
   <ToolkitProvider
   keyField="id"
-  data={ products }
+  data={ data1 }
   columns={ columns }
   
   search
